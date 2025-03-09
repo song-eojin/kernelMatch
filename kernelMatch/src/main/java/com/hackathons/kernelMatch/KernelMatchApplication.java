fe068f135c0f8d688f89fef17820bb8f63d9743f
song eojin <feshed528@gmail.com>
2025-03-09 14:40:01 +0900

    init : 스프링부트 초기 환경설정

---
+++ kernelMatch/src/main/java/com/hackathons/kernelMatch/KernelMatchApplication.java
@@ -0,0 +1,13 @@
+package com.hackathons.kernelMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KernelMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KernelMatchApplication.class, args);
+	}
+
+}
---
+++ kernelMatch/src/test/java/com/hackathons/kernelMatch/KernelMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathons.kernelMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KernelMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
